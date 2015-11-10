9ceb75db955852e16f77ddecf00ac2a2ce3801de
Xiaotao Wang <wangxiaotao868@gmail.com>
2015-11-10 09:18:25 +0800

    Only check for update when you are online

--- lib/__init__.py
+++ lib/__init__.py
@@ -7,18 +7,21 @@ import xmlrpclib
 from pkg_resources import parse_version as V
 
 __author__ = 'XiaoTao Wang'
-__version__ = '0.2.5'
+__version__ = '0.2.5-r1'
 __license__ = 'GPLv3+'
 
 ## Check for update
-pypi = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
-available = pypi.package_releases('TADLib')
-if V(__version__) < V(available[0]):
-    print '*'*75
-    print 'Version %s is out of date, Version %s is available.' % (__version__, available[0])
-    print 'Run `pip install -U TADLib` or `easy_install -U TADLib` for update.'
-    print
-    print '*'*75
+try:
+    pypi = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
+    available = pypi.package_releases('TADLib')
+    if V(__version__) < V(available[0]):
+        print '*'*75
+        print 'Version %s is out of date, Version %s is available.' % (__version__, available[0])
+        print 'Run `pip install -U TADLib` or `easy_install -U TADLib` for update.'
+        print
+        print '*'*75
+except:
+    pass
 
 Me = __file__
 
