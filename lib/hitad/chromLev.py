c7dfde42585061e31524e68e6a5f1e4afb63f388
Xiaotao Wang <wangxiaotao868@163.com>
2018-01-04 21:29:47 +0800

    try to support multi replicates again

--- lib/hitad/chromLev.py
+++ lib/hitad/chromLev.py
@@ -1830,6 +1830,7 @@ class MultiReps(DomainAligner):
         for k in sorted(base):
             tl, ql = base[k].info
             topobounds = {}  # bound positions should be passed down
+            new = self.intersect(k[0],k[1])
             if len(tl)==len(ql)==1:
                 d = tuple(tl[0])
                 topobounds[d] = tg.Domains[d]
@@ -1839,7 +1840,7 @@ class MultiReps(DomainAligner):
             else:
                 for d in map(tuple, tl):
                     topobounds[d] = tg.Domains[d]
-                    self._updateTopo(topobounds, d, [])
+                    self._updateTopo(topobounds, d, new)
 
             for lv in base[k]:
                 for t in base[k][lv]:
