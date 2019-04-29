f2af252021005f932f715cb596b3d8af3eb72fb4
Xiaotao Wang <wangxiaotao686@gmail.com>
2019-04-29 16:36:49 -0400

    module improvement and test

--- tadlib/domaincaller/chromLev.py
+++ tadlib/domaincaller/chromLev.py
@@ -8,6 +8,7 @@ Created on Tue May 31 15:47:34 2016
 from __future__ import division
 import numpy as np
 from tadlib.hitad import chromLev
+import copy
 
 chromCore = chromLev.Chrom
 
@@ -46,3 +47,115 @@ class Chrom(chromCore):
         self.calDI(self.windows, 0)
         self.splitChrom(self.DIs)
         self.domains = self.getDomainList(self.minCore(self.regionDIs))
+    
+    def DIsplit(self, arr):
+
+        intervals = []
+        # runs of negative values
+        tmp = set(np.where(arr>=0)[0])
+        breaks = copy.deepcopy(tmp)
+        for b in tmp:
+            breaks.add(b+1)
+        ori = np.split(arr, sorted(breaks))
+        start = 0
+        for a in ori:
+            if (len(a) > 1) and (a[0] < 0):
+                rb = start+len(a)
+                si = np.where(a[:-1]*5<a[1:])[0] # default factor: 5
+                if si.size > 0:
+                    lb = start + si[0]
+                    if rb - lb > 1:
+                        intervals.append((lb, rb))
+            start += len(a)
+        
+        # runs of positive values
+        tmp = set(np.where(arr<=0)[0])
+        breaks = copy.deepcopy(tmp)
+        for b in tmp:
+            breaks.add(b+1)
+        ori = np.split(arr, sorted(breaks))
+        start = 0
+        for a in ori:
+            if (len(a) > 1) and (a[0] > 0):
+                lb = start
+                ei = np.where(a[1:]*5>a[:-1])[0]
+                if ei.size > 0:
+                    rb = start + ei[-1] + 2
+                    if rb - lb > 1:
+                        intervals.append((lb, rb))
+            start += len(a)
+        
+        intervals.sort()
+        
+        return intervals
+    
+    def refine_bounds(self, seq, b):
+
+        subseq = seq[b[0]:b[1]]
+        parse = self.DIsplit(subseq)
+        starts = ()
+        min_DI = 0
+        ends = ()
+        max_DI = 0
+        for i in parse:
+            tmp = subseq[i[0]:i[1]].min()
+            if tmp < min_DI:
+                min_DI = tmp
+                starts = i
+            tmp = subseq[i[0]:i[1]].max()
+            if tmp > max_DI:
+                max_DI = tmp
+                ends = i
+        
+        if len(starts) and len(ends):
+            if starts[1] > ends[0]:
+                return []
+        
+        left = b[0]
+        if len(starts):
+            if starts == parse[0]:
+                left = b[0] + starts[0]
+            else:
+                tmp = subseq[:starts[0]].min()
+                if tmp >= 0:
+                    left = b[0] + starts[0]
+                else:
+                    fold = min_DI / tmp
+                    if fold > 2:
+                        left = b[0] + starts[0]
+        
+        right = b[1]
+        if len(ends):
+            if ends == parse[-1]:
+                right = b[0] + ends[1]
+            else:
+                tmp = subseq[ends[1]:].max()
+                if tmp <= 0:
+                    right = b[0] + ends[1]
+                else:
+                    fold = max_DI / tmp
+                    if fold > 2:
+                        right = b[0] + ends[1]
+        
+        return [left, right]
+        
+    def pipe(self, seq, start):
+
+        # bin-level domain (not base-pair-level domain!)
+        bounds = self._getBounds(self.viterbi(seq), junctions=['30'])
+        pairs = [[bounds[i], bounds[i+1]] for i in range(len(bounds)-1)]
+        domains = []
+        for b in pairs:
+            rb = self.refine_bounds(seq, b)
+            if len(rb):
+                # start, end, noise level, hierarchical level
+                tmp = [rb[0]+start, rb[1]+start, 0, 0]
+                domains.append(tmp)
+
+        return domains
+    
+    def viterbi(self, seq):
+        
+        path = [int(s.name) for i, s in self.hmm.viterbi(seq)[1][1:-1]]
+
+        return path
--- tadlib/domaincaller/genomeLev.py
+++ tadlib/domaincaller/genomeLev.py
@@ -68,7 +68,60 @@ class Genome(genomeCore):
         
         self.cool = lib
         
-    
+    def oriHMMParams(self):
+        """
+        Set initial parameters for the Hidden Markov Model (HMM).
+        
+        Attributes
+        ----------
+        HMMParams : dict
+            Has 3 keys: "A", state transition matrix, "B" (emission probabilities),
+            specifying parameters (Means, Variances, Weights) of the mixture
+            Gaussian distributions for each hidden state, and "pi", indicating
+            the hidden state weights. This dict will be updated after learning
+            procedure.
+        """
+        hmm = HiddenMarkovModel()
+        # GMM emissions
+        # 4 Hidden States:
+        # 0--start, 1--downstream, 2--upstream, 3--end
+        numdists = 3 # Three-distribution Gaussian Mixtures
+        var = 7.5 / (numdists - 1)
+        means = [[], [], [], []]
+        for i in range(numdists):
+            means[3].append(i * 7.5 / ( numdists - 1 ) + 2.5)
+            means[2].append(i * 7.5 / ( numdists - 1 ))
+            means[1].append(-i * 7.5 / ( numdists - 1 ))
+            means[0].append(-i * 7.5 / ( numdists - 1 ) - 2.5)
+        states = []
+        for i, m in enumerate(means):
+            tmp = []
+            for j in m:
+                tmp.append(NormalDistribution(j, var))
+            mixture = GeneralMixtureModel(tmp)
+            states.append(State(mixture, name=str(i)))
+        hmm.add_states(*tuple(states))
+
+        # Transmission matrix
+        #A = [[0., 1., 0., 0.],
+        #    [0., 0.5, 0.5, 0.],
+        #    [0., 0., 0.5, 0.5],
+        #    [1., 0., 0., 0.]]
+        hmm.add_transition(states[0], states[1], 1)
+        hmm.add_transition(states[1], states[1], 0.5)
+        hmm.add_transition(states[1], states[2], 0.5)
+        hmm.add_transition(states[2], states[2], 0.5)
+        hmm.add_transition(states[2], states[3], 0.5)
+        hmm.add_transition(states[3], states[0], 1)
+
+        #pi = [0.2, 0.3, 0.3, 0.2]
+        hmm.add_transition(hmm.start, states[0], 1)
+        hmm.add_transition(states[3], hmm.end, 1)
+
+        hmm.bake()
+
+        return hmm
+
     def train_data(self):
 
         seqs = []
@@ -148,6 +201,8 @@ class Genome(genomeCore):
             with open(tmpfil, 'rb') as source:
                 curChrom = pickle.load(source)
             for d in curChrom.domains:
+                if d[2] > 0.5:
+                    continue
                 self.Results.append([chrom, d[0], d[1], 0])
         
         log.debug('Add the DI column into cools ...')
